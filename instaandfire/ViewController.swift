85a4352ebaa94c1f43c1eedcbd2c0789a4de7edd
Jungtae Kim <jtk@jungtaeui-macbook-pro.local>
2017-02-19 17:36:14 +0900

    test commit

---
+++ instaandfire/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  instaandfire
+//
+//  Created by Jungtae Kim on 2017. 2. 19..
+//  Copyright © 2017년 Jungtae Kim. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        FIRAuth.auth()?.signInAnonymously(completion: { (user, error) in
+            if error != nil {
+                
+                print((error?.localizedDescription)!)
+                
+            } else {
+                
+                print((user?.uid)!)
+                
+                //test sss
+                //test again
+            }})
+        
+        let rootRef = FIRDatabase.database().reference()
+        let testRef = rootRef.child("message")
+        
+        let test = testRef.childByAutoId()
+        
+        let testDict = ["name": "jtk", "purpose" : "test"]
+        
+        test.setValue(testDict)
+        print(test)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
